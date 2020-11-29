4b1409ef1ea50d9858c4f7cad5e24d9b59b74cda
Will Ross <paxswill@paxswill.com>
2020-11-29 14:50:05 -0500

    Add systemd generator for /boot/netboot mounting, and add common utils

--- config/usr/share/cluster-netboot/load-config.sh
+++ config/usr/share/cluster-netboot/load-config.sh
@@ -12,7 +12,30 @@ CLUSTER_NFS_BASE_PATH=/mnt/cluster
 
 if [ -f /etc/defaults/cluster-netboot ]; then
     . /etc/defaults/cluster-netboot
+else
+    # Try finding the defaults config relative to this file (used when being
+    # sourced in initramfs from sysroot). This only works if this script is
+    # located at /usr/share/cluster-netboot
+    # If in bash, we can use BASH_SOURCE, otherwise we get to walk procfs.
+    if [ -n "$BASH" ]; then
+        _LOADER_FULL_PATH="$(realpath "$BASH_SOURCE")"
+    else
+        # realpath, sort, and tail are available through busybox in initramfs.
+        _LOADER_FULL_PATH="$(readlink -f \
+            "/proc/$$/fd/$(ls -1 /proc/$$/fd | sort -n | tail -1)"
+        )"
+    fi
+    _LOADER_RELATIVE_ROOT="$(realpath \
+        "$(dirname ${_LOADER_FULL_PATH})/../../.."
+    )"
+    if [ -f "${_LOADER_RELATIVE_ROOT}/etc/defaults/cluster-netboot" ]; then
+        . "${_LOADER_RELATIVE_ROOT}/etc/defaults/cluster-netboot"
+    fi
 fi
+
+# If we've booted a cluster node, there's sometimes a need to the actual,
+# resolved NFS server name.
+# TODO: Add a CLUSTER_NFS_SERVER_RESOLVED variable
 
 # The exported path to the root filesystems. If not set, defaults to
 # ${CLUSTER_NFS_BASE_PATH}/root
@@ -30,10 +53,10 @@ CLUSTER_NFS_ROOT_ARM64_PATH="${CLUSTER_NFS_ROOT_ARM64_PATH:-${CLUSTER_NFS_ROOT_P
 #CLUSTER_NFS_NETBOOT_PATH="${CLUSTER_NFS_BASE_PATH}/netboot"
 CLUSTER_NFS_NETBOOT_PATH="${CLUSTER_NFS_NETBOOT_PATH:-${CLUSTER_NFS_BASE_PATH}/netboot}"
 
-# The name of the U-Boot boot script defaults to "boot.scr".
+# The name of the U-Boot boot script defaults to "netboot.scr".
 # The path to the U-Boot boot script within the netboot root. Defaults to
 # "boot.scr" (meaning at the root of the netboot share).
-CLUSTER_UBOOT_SCRIPT_NAME="${CLUSTER_UBOOT_SCRIPT_NAME:-boot.scr}"
+CLUSTER_UBOOT_SCRIPT_NAME="${CLUSTER_UBOOT_SCRIPT_NAME:-netboot.scr}"
 
 # The kernel command line used for Raspberry Pis that can natively netboot.
 # If not set, the default is "net.ifnames=0 console=ttyS0,115200n8 root=/dev/nfs
