4a8cad2400c2f85b968beb0561a0d2b42032baa6
Will Ross <paxswill@paxswill.com>
2021-01-22 22:25:32 -0500

    Mount /var/machine-id over /etc/machine-id
    This seems to work better than the symlink method. This change also
    fixes the permissions on the initialize-var.sh initramfs script.

--- initramfs-tools/scripts/nfs-bottom/initialize-var.sh
+++ initramfs-tools/scripts/nfs-bottom/initialize-var.sh
@@ -20,4 +20,14 @@ set -e
 # systemd checks for this file and the contents shortly after *it* starts.
 if ! [ -f "${rootmnt}/var/machine-id" ]; then
 	printf "uninitialized\n" > "${rootmnt}/var/machine-id"
+	log_success_msg "machine-id set to 'uninitialized'"
 fi
+
+# Mount the node-specific machine=id over the paces where systemd and dbus
+# expect it to be
+mount -o bind "${rootmnt}/var/machine-id" "${rootmnt}/etc/machine-id"
+if ! [ -e "${rootmnt}/var/lib/dbus/machine-id" ]; then
+	ln -s ../../machine-id "${rootmnt}/var/lib/dbus/machine-id"
+	log_success_msg "Linked dbus machine-id to machine-id"
+fi
+log_success_msg "Bound machine-id to /etc/machine-id"
